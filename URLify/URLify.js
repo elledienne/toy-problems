9480d1d28692c4175fce36e642fd4f29098fb76c
Lorenzo De Nobili <elledienne@gmail.com>
2016-01-12 17:27:39 -0800

    Moved files

--- URLify/URLify.js
+++
@@ -1,5 +0,0 @@
-// Replace all the spaces is a string with '%20'
-
-var URLify = function (url) {
-  return url.split(' ').join('%20');
-}
--- isUnique/isUnique.js
+++
@@ -1,7 +0,0 @@
-// Determine if a string has all unique characters
-
-var isUnique = function (string) {
-  return string.split('').sort().every(function (val, index, arr) {
-    return index > 0 ? val !== arr[index-1] : true;
-  })
-}
