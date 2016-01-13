7f17fa9511c35d3c13687f2b8b3e465796d4c0d1
Lorenzo De Nobili <elledienne@gmail.com>
2016-01-12 17:26:52 -0800

    Add demo test

---
+++ test/test.spec.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+
+describe("A test suite", function() {
+   
+  beforeEach(function() {
+
+  });
+
+  afterEach(function() {
+
+  });
+
+  it('should fail', function() {
+    expect(true).to.be.true;
+  });
+  
+});
