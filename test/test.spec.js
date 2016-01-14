4389465a7edc554b3d69ebdab370c6643e2f861d
Lorenzo De Nobili <elledienne@gmail.com>
2016-01-14 11:07:19 -0800

    Add stringCompression tests

--- test/test.spec.js
+++ test/test.spec.js
@@ -14,7 +14,7 @@ describe("Test for: isUnique", function () {
   
 });
 
-describe("Test for: oneWay", function () {
+describe('Test for: oneWay', function () {
 
   xit('Should return false for strings which length difference is grater than 2', function () {
     expect(oneAway('pale', 'palese')).to.be.false;
@@ -42,6 +42,30 @@ describe("Test for: oneWay", function () {
 
   it('Should return true for strings that are equal', function () {
     expect(oneAway('lorenzo', 'lorenzo')).to.be.true;
-  })
+  });
 
+});
+
+describe('Test for: stringCompression', function () {
+
+  it('Should return a string that is shorter than the original', function () {
+    var string1 = 'aabcccccaaa';
+    var compressed1 = stringCompression(string1);
+
+    expect(compressed1.length < string1.length).to.be.true;
+  });
+
+  it('Should return the original string if the compressed one is not shorter', function () {
+    var string2 = 'aabbccddee';
+    var compressed2 = stringCompression(string2);
+
+    var string3 = 'abcdefghil';
+    var compressed3 = stringCompression(string3);
+
+    expect(compressed2 === string2 && compressed3 === string3).to.be.true;
+  });
+
+  it('Should work with lowercase and uppercase letters', function () {
+    expect(stringCompression('aaaAAbBBBBzzzzzzZ')).to.be.equal('a3A2b1B4z6Z1')
+  })
 });
